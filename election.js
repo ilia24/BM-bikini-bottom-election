e8b257d501d7c5771aa6bf0025d10bbf752d1ffd
ilia24 <iliademertchian24@gmail.com>
2017-05-04 00:53:21 -0400

    add base ajax call to election server

--- election.js
+++ election.js
@@ -1,5 +1,15 @@
-$(document).ready(function() {
+$(function() {
 
-  // Imagination!
+  $.ajax({
+    method: 'GET',
+    url: 'https://bb-election-api.herokuapp.com/',
+    dataType: 'json'
+  }).done(function(data){
+    console.log(data);
+    $('#greeting-list').prepend(data)
+  }).fail(function(){
+    console.log('form submission failed')
+  }).always(function(){
+  });
 
 });
