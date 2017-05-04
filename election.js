6be2818ce76e81926fbfb91cc37cdccc38fda0d0
ilia24 <iliademertchian24@gmail.com>
2017-05-04 01:13:42 -0400

    Add ajax call to retrieve candidate data and append it to a candidate HTML list

--- election.js
+++ election.js
@@ -5,8 +5,11 @@ $(function() {
     url: 'https://bb-election-api.herokuapp.com/',
     dataType: 'json'
   }).done(function(data){
-    console.log(data);
-    $('#greeting-list').prepend(data)
+    console.log('Ajax call for candidates successful');
+    for (var c = 0; c < data.candidates.length; c++) {
+      candid = data.candidates[c]
+      $('<li>').html('Candidate ' + (c + 1) + ': ' + candid.name + " Votes: " + candid.votes).appendTo('#candidatelist');
+    };
   }).fail(function(){
     console.log('form submission failed')
   }).always(function(){
